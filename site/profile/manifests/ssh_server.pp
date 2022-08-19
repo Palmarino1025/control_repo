0cde141f9e87ca07cfc8e6a3af9eecde774679f7
Robert P <106127288+Palmarino1025@users.noreply.github.com>
2022-08-19 15:26:59 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC+HtgyKUBXSFOIZtQn6RZhXuLibuZa6ND8pb4OJUkj67YiuwoDRgMymOoDrQn95UVt2/EoU0wHCrrU8MoreJ6Z3UY86fZ05nwlXOECjj3FWMItkS7fyK8HLCkjNgbtiu8yYj8oSn/wkkd/BN+eSGDONorWL4xdwCNiPaGvQq2nNwClWtRH//S5l7Xps+GTRUhSNZnWWHbB/161TiY8Fa1CUgyPEw6ZzTRTLgEq/6j3KSWC+OO9VGkWdhsul59XggebK7luhuaO37RAFelvesVu6AsapaReVVrLDDBI8BW5sxTk8nALni1G1+N4sjRriE5fhfQBoY2a4hORLhaywcCp',
 	}  
 }
